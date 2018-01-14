c2b2f9ff2e62026b26250b517e05f9d89680bac7
qatoe1991 <eltjona@bynder.com>
2018-01-14 19:23:50 +0100

    Update files names

--- setup.py
+++ setup.py
@@ -1,26 +1,32 @@
-from setuptools import setup, find_packages
+from setuptools import setup
 from AllureReportLibrary.version import VERSION
 
 setup(
-    name = 'robotframework-allurereport',
-    packages = ['AllureReportLibrary'],
-    version = VERSION,
-    description = 'Allure Reporting Adaptor for Robot Framework',
-    long_description = "The Allure Adaptor for Robot Framework is a Library that can be included in the Robot scripts to generate compatible XML files which can then be used to generate the Allure HTML reports.",
-    author = 'Eltjona Qato, Anne Kootstra', 
-    author_email = 'qatoeltjona@gmail.com, kootstra@hotmail.com',
-    url = 'https://github.com/kootstra/robotframework-allurereport',
-    keywords = ['allure', 'robotframework', 'reporting'],
-    license      = 'MIT License',
-    platforms    = 'any',
-    download_url = 'https://github.com/kootstra/robotframework-allurereport/tarball/' + VERSION,
-    classifiers = [
-                        "License :: OSI Approved :: MIT License",
-                        "Operating System :: OS Independent",
-                        "Programming Language :: Python",
-                        "Topic :: Software Development :: Testing",
-                        "Development Status :: 4 - Beta"
-                  ],
+    name='robotframework-allurereport',
+    packages=['AllureReportLibrary'],
+    version=VERSION,
+    description='Allure Reporting Adaptor for Robot Framework',
+    long_description=(
+        'The Allure Adaptor for Robot Framework is a Library that can be'
+        ' included in the Robot scripts to generate compatible XML files which'
+        ' can then be used to generate the Allure HTML reports.'
+    ),
+    author='Eltjona Qato, Anne Kootstra',
+    author_email='qatoeltjona@gmail.com, kootstra@hotmail.com',
+    url='https://github.com/kootstra/robotframework-allurereport',
+    keywords=['allure', 'robotframework', 'reporting'],
+    license='MIT License',
+    platforms='any',
+    download_url=(
+        'https://github.com/kootstra/robotframework-allurereport/tarball/1.1.1'
+    ),
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Topic :: Software Development :: Testing",
+        "Development Status :: 4 - Beta"
+    ],
     install_requires=[
         "lxml>=3.2.0",
         "namedlist",
